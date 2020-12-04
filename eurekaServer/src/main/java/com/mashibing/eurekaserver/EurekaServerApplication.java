2abc58652790c818637e7ebbacbf384c3fc328a3
hx1098 <1098269542@qq.com>
2020-12-04 22:25:02 +0800

    1

---
+++ eurekaServer/src/main/java/com/mashibing/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,23 @@
+package com.mashibing.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author hx   <br>
+ * @Title: <br>
+ * @Description: <br>
+ * @date 2020/12/3 21:45
+ * 123123123
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
