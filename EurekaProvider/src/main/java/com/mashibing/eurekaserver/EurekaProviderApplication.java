a9c3708a6731c9ae67f9afa9663025e895b55349
hx1098 <1098269542@qq.com>
2020-12-13 22:32:26 +0800

    上报节点

--- EurekaProvider/src/main/java/com/mashibing/eurekaserver/EurekaProviderApplication.java
+++ EurekaProvider/src/main/java/com/mashibing/eurekaserver/EurekaProviderApplication.java
@@ -11,8 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-public class EurekaClientApplication {
+public class EurekaProviderApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaClientApplication.class);
+        SpringApplication.run(EurekaProviderApplication.class);
     }
 }
