c672eea051ebf7411413fa8475b4f84ea6615b52
hx1098 <1098269542@qq.com>
2020-12-20 23:33:53 +0800

    server添加配置 使 provider能够注册到server上面

---
+++ EurekaServer/src/main/java/com/mashibing/eurekaserver/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.mashibing.eurekaserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author hx
+ * @createTime 2020/12/20 23:11
+ * @option 安全认证
+ * @description
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // TODO Auto-generated method stub
+        http.csrf().disable();//关闭csrf
+        super.configure(http);
+    }
+
+}
