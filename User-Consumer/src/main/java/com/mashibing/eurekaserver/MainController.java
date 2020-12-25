63264b61b3490fb46c2748e884ffae2fe7e70bc9
hx1098 <1098269542@qq.com>
2020-12-25 22:13:43 +0800

    xxxx

--- User-Consumer/src/main/java/com/mashibing/eurekaserver/MainController.java
+++ User-Consumer/src/main/java/com/mashibing/eurekaserver/MainController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     @Autowired
-    public UserApi userApi;
+    public ConsumerAPI userApi;
 
     @GetMapping("/alive")
     public String Alive(){
