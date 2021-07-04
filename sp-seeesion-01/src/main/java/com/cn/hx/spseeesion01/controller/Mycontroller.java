6be9c43da22df3b59600e6334b8f18a64be9ec67
hx1098 <1098269542@qq.com>
2021-07-04 19:58:58 +0800

    springsession 链接redis, 可以看到redis中有很多key在里面

---
+++ sp-seeesion-01/src/main/java/com/cn/hx/spseeesion01/controller/Mycontroller.java
@@ -0,0 +1,26 @@
+package com.cn.hx.spseeesion01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hx
+ * @version 1.0.0
+ * @createTime 2021/7/4 19:46
+ * @option
+ * @description
+ * @editUser hx
+ * @editTime 2021/7/4 19:46
+ * @editDescription
+ */
+@RestController
+public class Mycontroller {
+
+    @GetMapping
+    public String getString(){
+        return "xx00";
+    }
+
+
+}
