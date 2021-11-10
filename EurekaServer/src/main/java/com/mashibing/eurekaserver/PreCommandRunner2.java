19efa2cd4bd744f696ff465534b39f405199c875
hx1098 <1098269542@qq.com>
2021-11-10 16:45:48 +0800

    测试springboot启动时执行的方法, 和 两个方法执行的顺序(order)

---
+++ EurekaServer/src/main/java/com/mashibing/eurekaserver/PreCommandRunner2.java
@@ -0,0 +1,25 @@
+package com.mashibing.eurekaserver;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author hx
+ * @version 1.0.0
+ * @createTime 2021/11/10 16:40
+ * @description
+ * @editUser hx
+ * @editTime 2021/11/10 16:40
+ * @editDescription 测试springboot 先执行的方法
+ */
+@Component
+@Order(2)
+public class PreCommandRunner2 implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("=============> 自定的第二个方法开始执行了222....");
+    }
+}
